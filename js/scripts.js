76955543d4b37d659cf3d1042635f6fa2ffb0441
Cody Brubaker <cody.brubaker@gmail.com>
2016-10-28 10:10:10 -0700

    replace numbers with multiples of 5 or 15 with pong or ping-pong

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,29 @@
 var counter = 0
 var emptyArray = []
 var ping = "ping";
+var pong = "pong";
+var pingPong = "ping-pong";
 
-var pingPong = function(input) {
+
+var pingPongFnc = function(input) {
 debugger;
+  emptyArray = [];
   for ( var index = 0; index < input; index++) {
     counter += 1;
     emptyArray.push(counter);
 
-  if (emptyArray[index] % 3 === 0) {
+  if (emptyArray[index] % 15 === 0) {
+      emptyArray.pop();
+      emptyArray.push(pingPong);
+    }
+    else if (emptyArray[index] % 3 === 0) {
       emptyArray.pop();
       emptyArray.push(ping);
     }
-
+    else if (emptyArray[index] % 5 === 0) {
+      emptyArray.pop();
+      emptyArray.push(pong);
+    }
   }
   return emptyArray
 }
@@ -23,7 +34,7 @@ $(document).ready(function(){
     event.preventDefault();
 debugger;
     var userInput = parseInt($("#input").val());
-    var result = pingPong(userInput);
+    var result = pingPongFnc(userInput);
     $("ul").append("<li>" + result + "</li>");
 
   });
